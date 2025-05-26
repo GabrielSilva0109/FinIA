0a60bc82024d8028e23dc705a827ff365a6633ac
Gabriel <silvagabriel0109@gmail.com>
2025-05-26 12:48:04 -0300

    Server on

--- backend/main.py
+++ backend/main.py
@@ -5,3 +5,8 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "FinAI backend está funcionando 🚀"}
+
+
+@app.get("/seila")
+def read_root():
+    return {"message": "FinAI DALEEEEEEEEEE"}
