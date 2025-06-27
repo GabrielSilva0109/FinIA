25b7413fa126517b389953da7fccb60593553c2a
Gabriel <silvagabriel0109@gmail.com>
2025-06-27 16:27:33 -0300

    Feat: Cors

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI, Query
+from fastapi.middleware.cors import CORSMiddleware
 import yfinance as yf
 from typing import Optional
 from logic import analyze, analyze_all
 
 app = FastAPI()
+
+# Adicione isto para permitir CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:8080"], 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def root():
